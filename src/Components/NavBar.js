455e282678a1ecf1832c02b21c5d781ca2888342
Atanas Todorov <atodorov98@outlook.com>
2022-01-12 13:38:38 +0200

    navigation bar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,10 @@
+
+
+export default function NavBar(){
+    return(
+        <nav>
+            <img src="/earth.png" className="nav--logo"/>
+            <h1>My travel journal</h1>
+        </nav>
+    )
+}
